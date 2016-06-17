9a8927b58f5d1ab2362ad86e266125c70d9c577c
kelbyturk2 <kelbyturk2@gmail.com>
2016-06-17 14:16:34 -0400

    Added mousdown & mouseup animation for images

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+      $('.ryu-still').hide();
+      $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+      $('.ryu-still').show();
+      $('.ryu-ready').hide();
+  })
+  .mousedown(function() {
+      $('.ryu-ready').hide();
+      $('.ryu-throwing').show();
+      $('.hadouken').show();
+    // play hadouken sound
+    // show hadouken and animate it to the right of the screen
+  })
+  .mouseup(function() {
+      $('.ryu-ready').show();
+      $('.ryu-throwing').hide();
+    // ryu goes back to his ready position
+  });
+});
